0c5a7e9183ba3242d2aa32651b150947145b79ad
임채성 <75980875+puleugo@users.noreply.github.com>
2023-01-13 22:46:50 +0900

    [Gold V] Title: 로봇 청소기, Time: 76 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+visited = [[0] * m for _ in range(n)]
+r,c,d = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+visited[r][c] = 1
+cnt = 1
+
+while 1:
+    flag = 0
+    for _ in range(4):
+        nx = r + dx[(d+3)%4]
+        ny = c + dy[(d+3)%4]
+        d = (d+3)%4
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+            if visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                cnt += 1
+                r = nx
+                c = ny
+                flag = 1
+                break
+    if flag == 0:
+        if graph[r-dx[d]][c-dy[d]] == 1:
+            print(cnt)
+            break
+        else:
+            r,c = r-dx[d],c-dy[d]
